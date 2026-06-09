e5d74d5c1d1863996d8834783a6c3e1bbbcab9b6
Josh Margulis <josh.margulis@gmail.com>
2026-06-08 19:55:59 -0700

    jmargulis: OIDC SSO capabilities

---
+++ server/public/model/oidc.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// jmargulis: This file defines the OIDC-related configuration settings and defaults.
+
+package model
+
+const (
+	UserAuthServiceOIDC = "oidc"
+)
